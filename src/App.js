4b31f90fbcd97ff80031d48dfa77f788bce951b7
Werayootk <werayoot5800@gmail.com>
2021-08-13 08:13:52 +0700

    Finished Case2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import StateCase1 from './useStateCase1/StateCase1';
+import StateCase2 from './useStateCase2/StateCase2';
 
 function App() {
   return (
     <div className="App">
-      <StateCase1 />
+      <StateCase2 />
     </div>
   );
 }
